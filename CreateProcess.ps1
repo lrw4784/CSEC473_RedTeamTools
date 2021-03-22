0bbdf55830ac39d3d7b8daf05852018d5b559b85
lrw4784 <36006133+lrw4784@users.noreply.github.com>
2021-03-22 13:47:34 -0400

    Update CreateProcess.ps1

--- CreateProcess.ps1
+++ CreateProcess.ps1
@@ -1,3 +1,5 @@
+#Elevates privliege then loops indefinetly to create notepad instances.
+#The nested loop also used to only create 20 instacnes just to be a little fair to the blue team.
 set-executionpolicy -Scope CurrentUser bypass
 while($true)
 {
